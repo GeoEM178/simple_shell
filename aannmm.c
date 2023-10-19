3b70a36ee368b0454815992dea201a71a9f0640a
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-10-19 10:42:31 -0700

    added new Modifications

--- aannmm.c
+++ aannmm.c
@@ -1,61 +1,61 @@
 #include "header.h"
 
 /**
- * path_amrot -  Search In $PATH for executable amrhjkx
- * @amrot: Parsed dkhkhar
- * Return: 0 on success or  1 on failure  0
+ * path_cmd -  Search In $PATH for executable amrhjkx
+ * @amrot: array of character
+ * Return: 0 if success or  1 if failure  0
  */
 int path_cmd(char **amrot)
 {
-	char *path, *aikimal, *cmd_path;
-	struct stat buf;
+char *path, *aikimal, *cmd_path;
+struct stat buf;
 
-	path = _getenv("PATH");
-	aikimal = _strtok(path, ":");
-	while (aikimal != NULL)
-	{
-		cmd_path = build(*amrot, aikimal);
-		if (stat(cmd_path, &buf) == 0)
-		{
-			*amrot = _strdup(cmd_path);
-			free(cmd_path);
-			free(path);
-			return (0);
-		}
-		free(cmd_path);
-		aikimal = _strtok(NULL, ":");
-	}
-	free(path);
-	free(aikimal);
-	return (1);
+path = _getenv("PATH");
+aikimal = _strtok(path, ":");
+while (aikimal != NULL)
+{
+cmd_path = build(*amrot, aikimal);
+if (stat(cmd_path, &buf) == 0)
+{
+*amrot = _strdup(cmd_path);
+free(cmd_path);
+free(path);
+return (0);
+}
+free(cmd_path);
+aikimal = _strtok(NULL, ":");
+}
+free(path);
+free(aikimal);
+return (1);
 }
 
 /**
- * build - Build amrhjkx
- * @wahfFg: Executable amrhjkx
- * @aikimal: Directory conatining amrhjkx
+ * build - Build arguments for me
+ * @wahfFg: Executable amrhjkx function
+ * @aikimal: Directory conatining amrhjkx in user circle
  * Return: Parsed full path of amrhjkx or NULL if failed
  */
 char *build(char *wahfFg, char *aikimal)
 {
-	char *amrot;
-	size_t len;
+char *amrot;
+size_t len;
 
-	len = _strlen(aikimal) + _strlen(wahfFg) + 2;
-	amrot = malloc(sizeof(char) * len);
-	if (amrot == NULL)
-	{
-		free(amrot);
-		return (NULL);
-	}
+len = _strlen(aikimal) + _strlen(wahfFg) + 2;
+amrot = malloc(sizeof(char) * len);
+if (amrot == NULL)
+{
+free(amrot);
+return (NULL);
+}
 
-	memset(amrot, 0, len);
+memset(amrot, 0, len);
 
-	amrot = _strcat(amrot, aikimal);
-	amrot = _strcat(amrot, "/");
-	amrot = _strcat(amrot, wahfFg);
+amrot = _strcat(amrot, aikimal);
+amrot = _strcat(amrot, "/");
+amrot = _strcat(amrot, wahfFg);
 
-	return (amrot);
+return (amrot);
 }
 
 /**
@@ -65,33 +65,33 @@ char *build(char *wahfFg, char *aikimal)
  */
 char *_getenv(char *asmNam)
 {
-	size_t asmNam_len, aikimal_len;
-	char *aikimal;
-	int i, j, k;
+size_t asmNam_len, aikimal_len;
+char *aikimal;
+int i, j, k;
 
-	asmNam_len = _strlen(asmNam);
-	for (i = 0 ; environ[i]; i++)
-	{
-		if (_strncmp(asmNam, environ[i], asmNam_len) == 0)
-		{
-			aikimal_len = _strlen(environ[i]) - asmNam_len;
-			aikimal = malloc(sizeof(char) * aikimal_len);
-			if (!aikimal)
-			{
-				free(aikimal);
-				perror("unable to alloc");
-				return (NULL);
-			}
+asmNam_len = _strlen(asmNam);
+for (i = 0 ; environ[i]; i++)
+{
+if (_strncmp(asmNam, environ[i], asmNam_len) == 0)
+{
+aikimal_len = _strlen(environ[i]) - asmNam_len;
+aikimal = malloc(sizeof(char) * aikimal_len);
+if (!aikimal)
+{
+free(aikimal);
+perror("unable to alloc");
+return (NULL);
+}
 
-			j = 0;
-			for (k = asmNam_len + 1; environ[i][k]; k++, j++)
-			{
-				aikimal[j] = environ[i][k];
-			}
-			aikimal[j] = '\0';
+j = 0;
+for (k = asmNam_len + 1; environ[i][k]; k++, j++)
+{
+aikimal[j] = environ[i][k];
+}
+aikimal[j] = '\0';
 
-			return (aikimal);
-		}
-	}
-	return (NULL);
+return (aikimal);
+}
+}
+return (NULL);
 }
