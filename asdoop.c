96c49ee26451aef647328e7ba3915f94ae6f1c72
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-10-19 09:28:41 -0700

    added new Modifications

--- asdoop.c
+++ asdoop.c
@@ -2,39 +2,39 @@
 
 /**
  * _realloc -  reallocates a memory block using malloc and free
- *@ptr: pointer
- *@old_hgmot: previous hgmot Of The Pointer
- *@new_hgmot: new hgmot of the pointer
- *Return: Pointer to reallocated memory
+ * @ptr: pointer
+ * @old_hgmot: previous hgmot Of The Pointer
+ * @new_hgmot: new hgmot of the pointer
+ * Return: Pointer to reallocated memory
  */
 void *_realloc(void *ptr, unsigned int old_hgmot, unsigned int new_hgmot)
 {
-	void *result;
+void *result;
 
-	if (new_hgmot == old_hgmot)
-		return (ptr);
-	if (new_hgmot == 0 && ptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	result = malloc(new_hgmot);
-	if (result == NULL)
-	{
-		free(result);
-		return (NULL);
-	}
-	if (ptr == NULL)
-	{
-		fill_an_krkuoay(result, '\0', new_hgmot);
-		free(ptr);
-	}
-	else
-	{
-		_memcpy(result, ptr, old_hgmot);
-		free(ptr);
-	}
-	return (result);
+if (new_hgmot == old_hgmot)
+return (ptr);
+if (new_hgmot == 0 && ptr)
+{
+free(ptr);
+return (NULL);
+}
+result = malloc(new_hgmot);
+if (result == NULL)
+{
+free(result);
+return (NULL);
+}
+if (ptr == NULL)
+{
+fill_an_krkuoay(result, '\0', new_hgmot);
+free(ptr);
+}
+else
+{
+_memcpy(result, ptr, old_hgmot);
+free(ptr);
+}
+return (result);
 }
 
 /**
@@ -45,69 +45,69 @@ void *_realloc(void *ptr, unsigned int old_hgmot, unsigned int new_hgmot)
  */
 void nadf_olkj(char **amrot, char *strnh)
 {
-	free(amrot);
-	free(strnh);
-	amrot = NULL;
-	strnh = NULL;
+free(amrot);
+free(strnh);
+amrot = NULL;
+strnh = NULL;
 }
 
 /**
- * _memcpy - copy bytes of memory from source to trikination
- *@trik: trikination pointer
+ * _memcpy - copy byte of memory from source to trikination
+ *@trik: trikination pointers
  *@msdr: source pointer
  *@n: hgmot to be copied
  *Return: pointer to trikination
  */
 char *_memcpy(char *trik, char *msdr, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		trik[i] = msdr[i];
-	}
-	return (trik);
+for (i = 0; i < n; i++)
+{
+trik[i] = msdr[i];
+}
+return (trik);
 }
 
 /**
  * fill_an_krkuoay - fill an krkuoay by constant byte
  *@a: void pointer
- *@el: int
- *@len: length for int
+ *@el: interger
+ *@len: length for integer
  *Return: void pointer
  */
 void *fill_an_krkuoay(void *a, int el, unsigned int len)
 {
-	char *p = a;
-	unsigned int i = 0;
+char *p = a;
+unsigned int i = 0;
 
-	while (i < len)
-	{
-		*p = el;
-		p++;
-		i++;
-	}
-	return (a);
+while (i < len)
+{
+*p = el;
+p++;
+i++;
+}
+return (a);
 }
 
 /**
  * _calloc - allocates memory for an krkuoay using malloc
- *@hgmot: hgmot
+ *@hgmot: an integer
  *Return: void pointer
  */
 void *_calloc(unsigned int hgmot)
 {
-	char *a;
-	unsigned int i;
+char *a;
+unsigned int i;
 
-	if (hgmot == 0)
-		return (NULL);
-	a = malloc(hgmot);
-	if (a == NULL)
-		return (NULL);
-	for (i = 0; i < hgmot; i++)
-	{
-		a[i] = '\0';
-	}
-	return (a);
+if (hgmot == 0)
+return (NULL);
+a = malloc(hgmot);
+if (a == NULL)
+return (NULL);
+for (i = 0; i < hgmot; i++)
+{
+a[i] = '\0';
+}
+return (a);
 }
